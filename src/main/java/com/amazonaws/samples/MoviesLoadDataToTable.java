2e310658ad54c89891a8f91bfbab2f06d631ff47
lucyygao <42822456+lucyygao@users.noreply.github.com>
2020-05-27 17:03:25 -0700

    publish.

--- src/main/java/com/amazonaws/samples/MoviesLoadDataToTable.java
+++ src/main/java/com/amazonaws/samples/MoviesLoadDataToTable.java
@@ -13,7 +13,7 @@ package com.amazonaws.samples;
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
 */
-
+//test
 import java.io.File;
 import java.util.Iterator;
 
